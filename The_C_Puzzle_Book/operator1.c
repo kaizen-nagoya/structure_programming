83be6148eabad72fc34aa02a8bc9eabee0a328b8
kaizen_nagoya <kaizen@wh.commufa.jp>
2026-04-08 11:18:34 +0900

    Create operator1.c with examples of C operators
    Add operator1.c to demonstrate operator precedence in C

---
+++ The_C_Puzzle_Book/operator1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int x;
+    x = - 3 + 4 * 5 - 6; printf("%d\n", x);
+    x = 3 + 4 % 5 - 6; printf("%d\n", x);
+    x = -3 * 4 % -6 / 5; printf("%d\n", x);
+    x = ( 7 + 6 ) % 5 / 2; printf("%d\n", x);
+    return 0;
+}
+/* 
+11
+1
+0
+1
+  */

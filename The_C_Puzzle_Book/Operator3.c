ebb8722a5867e7a936d59d84f3cafd7fed75bd6c
kaizen_nagoya <kaizen@wh.commufa.jp>
2026-04-08 11:24:00 +0900

    Add Operator3.c with logical and arithmetic examples
    Implement a sample program demonstrating logical and arithmetic operations in C.

---
+++ The_C_Puzzle_Book/Operator3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define PRINT(int) printf("%d\n",int)
+
+int main() {
+    int x, y, z;
+
+    x = 2; y = 1; z = 0;
+    x = x && y || z; PRINT(x);
+    PRINT( x || ! y && z );
+
+    x = y = 1;
+    z = x ++ - 1; PRINT(x); PRINT(z);
+    z += - x ++ + ++ y; PRINT(x); PRINT(z);
+    z = x / ++ x; PRINT(z);
+}
+/*
+1
+1
+2
+0
+3
+0
+1
+  */

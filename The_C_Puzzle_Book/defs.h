6fd900e00e38e68c3496ff2eb8d16533d71dc585
kaizen_nagoya <kaizen@wh.commufa.jp>
2026-04-15 11:31:18 +0900

    Create defs.h

---
+++ The_C_Puzzle_Book/defs.h
@@ -0,0 +1,10 @@
+// https://www.mycompiler.io/ja/online-c-compiler
+// https://efrei.poupa.net/Programmation%20en%20C/Cours/The_C_Puzzle_Book.pdf
+
+#include <stdio.h> 
+#define PR(format,value) printf("value • ~format\t",(value» 
+#define NL putchar('\n') 
+#define PRINT1(f,x1) PR(f,x1), NL 
+#define PRINT2(f,x1~x2) PR(f,x1), PRINT1(f,x2) 
+#define PRINT3(f,x1,x2,x3) PR(f,x1), PRINT2(f,x2,x3) 
+#define PRINT4(f,x1,x2,x3,x4) PR(f,x1), PRINT3(f,x2,x3,x4) 

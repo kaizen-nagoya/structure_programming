2fd03bc93b0f54aaf8521488ec9f7f6a81fcb79b
kaizen_nagoya <kaizen@wh.commufa.jp>
2026-04-22 11:53:14 +0900

    Update ProgrammingStyle1.c

--- The_C_Puzzle_Book/ProgrammingStyle1.c
+++ The_C_Puzzle_Book/ProgrammingStyle1.c
@@ -1,44 +1,63 @@
-#include <stdio.h>
-#define A
-#define B
-#define C
-#define D
-#deifne E
-#define F
-#define OTHER
-#define DIGIT
-#define ALPHA
+// https://www.mycompiler.io/ja/online-c-compiler
+// https://efrei.poupa.net/Programmation%20en%20C/Cours/The_C_Puzzle_Book.pdf
+
+#include <stdio.h> 
+#define PR(format,value) printf(#value "= %" #format "\t", (value))
+#define NL putchar('\n') 
+#define PRINT1(f,x1) PR(f,x1), NL 
+#define PRINT2(f,x1, x2) PR(f,x1), PRINT1(f,x2) 
+#define PRINT3(f,x1,x2,x3) PR(f,x1), PRINT2(f,x2,x3) 
+#define PRINT4(f,x1,x2,x3,x4) PR(f,x1), PRINT3(f,x2,x3,x4) 
+
+#define A 0
+#define B 0
+#define B1 PRINT1(s,"B")
+#define C 0
+#define D PRINT1(s,"D")
+#define E PRINT1(s,"E")
+#define F PRINT1(s,"F")
+#define OTHER0 PRINT1(s,"OTHER0")
+#define OTHERa PRINT1(s,"OTHERa")
+#define OTHERS PRINT1(s,"OTHERS")
+#define DIGIT PRINT1(s,"DIGIT")
+#define ALPHA PRINT1(s,"ALPHA")
 
 int main() {
+   int c;
     while (A) { 
     if(B) continue; 
     C; 
     } 
     do { 
-        if( IA) continue; 
-        else B; 
+        if(!A) continue; 
+        else B1; 
         C; 
     } while(A); 
     
     if(A) 
-    else 
         if(B) 
-        else; 
-    if(B) 
-    else; 
-    if(C) D; 
-    else; 
-    if(C) E; 
-    else F; 
-    while ( (c=qetchar ( ) ) I .. ''\n' ) { 
-    if( c==' , ) continue; 
-    if( c==',\t' ) continue; 
-    if( c<'O' ) return(OTHER); 
+            if(C) D; 
+            else;
+        else;
+    else
+        if(B) 
+            if(C) E; 
+            else F; 
+        else;
+
+    while ( (c=getchar() ) !='\n' ) { 
+    if( c==' ' ) continue; 
+    if( c=='\t' ) continue; 
+    if( c<'O' ) return(OTHER0); 
     if( c<='9' ) return(DIGIT); 
-    if( c<'a' ) return(OTHER); 
+    if( c<'a' ) return(OTHERa); 
     if( c<='z' ) return(ALPHA); 
 //(Programming Style 1.1) 
 //(Programming Style 1.2) 
 //(Programming Style 1.3) 
-    return(OTHER); 
+    return(OTHERS); 
+    }
 }
+/*
+"OTHER0"= OTHER0
+*/

f40c607e0ebc3e38faaa2ba7f893a849d450fd55
kaizen_nagoya <kaizen@wh.commufa.jp>
2026-04-22 11:18:31 +0900

    Update ControlFlow1.c

--- The_C_Puzzle_Book/ControlFlow1.c
+++ The_C_Puzzle_Book/ControlFlow1.c
@@ -2,68 +2,71 @@
 // https://efrei.poupa.net/Programmation%20en%20C/Cours/The_C_Puzzle_Book.pdf
 
 #include <stdio.h> 
-#define PR(format,value) printf(#value "= %" #format "\t",(value))
-#define NL putchar('\n')
-#define PRINT1(f,x1) PR(f,x1), NL
-#define PRINT2(f,x1,x2) PR(f,x1), PRINT1(f,x2)
-#define PRINT3(f,x1,x2,x3) PR(f,x1), PRINT2(f,x2,x3)
-#define PRINT4(f,x1,x2,x3,x4) PR(f,x1), PRINT3(f,x2,x3,x4)
+#define PR(format,value) printf(#value "= " format "\t", (value))
+#define NL putchar('\n') 
+#define PRINT1(f,x1) PR(f,x1), NL 
+#define PRINT2(f,x1, x2) PR(f,x1), PRINT1(f,x2) 
+#define PRINT3(f,x1,x2,x3) PR(f,x1), PRINT2(f,x2,x3) 
+#define PRINT4(f,x1,x2,x3,x4) PR(f,x1), PRINT3(f,x2,x3,x4) 
 
-int main() {
-    int x, y, z;
-
-    x = y = 0;
-    while( y < 10 ) ++y; x += y;
-    PRINT2(d,x,y);
-
-    x = y = 0;
-    while( y < 10 ) x += ++y;
-    PRINT2(d,x,y);
+int main() { 
+    int x, y= 1, z;
+    /*if(1){
+        printf("true\n");
+    }*/
+    /*if(0){
+        printf("false\n");
+    }*/
+    //↑if(0)だと条件を満たしていないことになっている？ためスルー
+    //↓は↑の挙動と同じ
+    /*if(z = 0){
+        printf("あああ\n");
+    }*/
     
-    y = 1 ;
-    while ( y < 10 ){
-        x = y++; z = ++y;
-    }
-    PRINT3(d, x, y, z);
+    if( y!=0 ) x=5; 
+    PRINT1 ("%d",x);
     
-    for( y = 1; y < 10; y++ ) x = y;
-    PRINT2(d, x, y);
-
-    for( y = 1; (x = y) < 10; y++)
-    PRINT2(d, x, y);
-
-    for( x = 0, y = 1000; y > 1; x++, y /= 10 )
-        PRINT2 (d, x, y) ;
+    if( y==0 ) x=3; 
+    else x=5; 
+    PRINT1 ("%d",x);
     
+    x=1; 
+    if( y<0 ) if( y>0 ) x=3; 
+    else x=5; 
+    /*if (y < 0) {
+        if (y > 0) {
+            x = 3;
+        } 
+        else {
+            x = 5;
+        }
+    }*/
+    PRINT1 ("%d",x);
+    
+    //PRINT1 ("%d",y); 
+    //↑何もいじっていなければ y = 1
+    
+    if( z=y<0 ) x=3;
+    else if( y==0 ) x=5; 
+    else x=7;
+    PRINT2("%d",x,z);
+    
+    if( z=(y==0) ) x=5; x=3;
+    PRINT2("%d",x,z); 
+
+    //PRINT3("%d",x,y,z);
+    //↑x= 3	y= 1　z= 0
+    
+    if( x=z=y ); x=3; 
+    //z=yののち、x=z?
+    PRINT2("%d",x,z);
 }
-
 /*
-実行結果
-x= 10	y= 10	
-x= 55	y= 10	
-x= 9	y= 11	z= 11	
-x= 9	y= 10	
-x= 1	y= 1	
-x= 2	y= 2	
-x= 3	y= 3	
-x= 4	y= 4	
-x= 5	y= 5	
-x= 6	y= 6	
-x= 7	y= 7	
-x= 8	y= 8	
-x= 9	y= 9	
-x= 0	y= 1000	
-x= 1	y= 100	
-x= 2	y= 10	
-
-
-理想結果
-x = 10  Y = 10 
-x = 55  y = 10 
-x = 9   y = 11  z = 11 
-x = 9   y = 10 
-x = 10  y = 10 
-x = 0   y = 1000
-x = 1   y = 100
-x = 2   y = 10
+x= 5	
+x= 5	
+x= 1	
+x= 7	z= 0	
+x= 3	z= 0	
+x= 3	z= 1	
 */
+

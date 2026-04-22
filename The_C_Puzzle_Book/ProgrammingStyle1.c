795b1a87bd81f138b6e8b40168307a6a9c1ec93c
kaizen_nagoya <kaizen@wh.commufa.jp>
2026-04-22 10:40:23 +0900

    Create ProgrammingStyle1.c

---
+++ The_C_Puzzle_Book/ProgrammingStyle1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#define A
+#define B
+#define C
+#define D
+#deifne E
+#define F
+#define OTHER
+#define DIGIT
+#define ALPHA
+
+int main() {
+    while (A) { 
+    if(B) continue; 
+    C; 
+    } 
+    do { 
+        if( IA) continue; 
+        else B; 
+        C; 
+    } while(A); 
+    
+    if(A) 
+    else 
+        if(B) 
+        else; 
+    if(B) 
+    else; 
+    if(C) D; 
+    else; 
+    if(C) E; 
+    else F; 
+    while ( (c=qetchar ( ) ) I .. ''\n' ) { 
+    if( c==' , ) continue; 
+    if( c==',\t' ) continue; 
+    if( c<'O' ) return(OTHER); 
+    if( c<='9' ) return(DIGIT); 
+    if( c<'a' ) return(OTHER); 
+    if( c<='z' ) return(ALPHA); 
+//(Programming Style 1.1) 
+//(Programming Style 1.2) 
+//(Programming Style 1.3) 
+    return(OTHER); 
+}
